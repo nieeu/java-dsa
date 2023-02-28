b5572336ccafd3b7f4a920532a6b70db187037f0
nieeu <kaitoukidz2001@gmail.com>
2023-02-28 13:36:34 +0700

    commit

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+import java.util.Arrays;
+
+class Array {
+    public static void main(String[] args)
+    {
+        double[ ] measurements = new double[1000];
+        System.out.println(measurements);
+    }
+}
